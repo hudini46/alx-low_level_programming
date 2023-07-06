7e2917157ad2400941f75c7017fe329267577306
hudini46 <owenkiilu37@gmail.com>
2023-07-06 02:33:13 -0700

    new

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * Return: 1 for success, -1 for failure
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
+}
+
